38897c33be36ca8c022e7003aa9f3c50b4899108
huhen <9335143+huhen@users.noreply.github.com>
2025-10-12 18:08:31 +0000

    Impl

--- src/metric_impl/code_lines_count.cpp
+++ src/metric_impl/code_lines_count.cpp
@@ -2,23 +2,39 @@
 
 #include <unistd.h>
 
+#include "utils.hpp"
 #include <algorithm>
-#include <array>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <filesystem>
-#include <fstream>
 #include <functional>
-#include <iostream>
 #include <ranges>
-#include <sstream>
 #include <string>
-#include <variant>
-#include <vector>
+#include <unordered_set>
 
 namespace analyser::metric::metric_impl {
 
-// здесь ваш код
+std::string CodeLinesCountMetric::Name() const { return "code_lines_count"; }
+
+MetricResult::ValueType CodeLinesCountMetric::CalculateImpl(const function::Function &f) const {
+    static constexpr std::string_view comment_pattern = "(comment";
+
+    auto res = f.ast | vs::split('\n') | vs::transform([](const auto &range) -> std::optional<int> {
+                   const auto str_view = std::string_view{range};
+
+                   static const std::boyer_moore_searcher searcher(comment_pattern.begin(), comment_pattern.end());
+
+                   if (std::search(str_view.begin(), str_view.end(), searcher) == str_view.end()) {
+                       if (auto start_pos = str_view.find('['); start_pos != std::string_view::npos) {
+                           if (auto comma = str_view.find(',', start_pos); comma != std::string_view::npos) {
+                               return ToInt(str_view.substr(start_pos + 1, comma - start_pos - 1));
+                           }
+                       }
+                   }
+
+                   return std::nullopt;
+               }) |
+               vs::filter([](const auto &opt) { return opt.has_value(); }) |
+               vs::transform([](const auto &opt) { return opt.value(); }) | rs::to<std::unordered_set>();
+
+    return static_cast<int>(res.size());
+}
 
 }  // namespace analyser::metric::metric_impl
