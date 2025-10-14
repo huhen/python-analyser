924dca685ede4bb289a5189a3627b949d8f3d11e
huhen <9335143+huhen@users.noreply.github.com>
2025-10-14 17:27:45 +0000

    update

--- src/metric_impl/code_lines_count.cpp
+++ src/metric_impl/code_lines_count.cpp
@@ -3,8 +3,6 @@
 #include <unistd.h>
 
 #include "utils.hpp"
-#include <algorithm>
-#include <functional>
 #include <ranges>
 #include <string>
 #include <unordered_set>
@@ -14,18 +12,14 @@ namespace analyser::metric::metric_impl {
 std::string CodeLinesCountMetric::Name() const { return "code_lines_count"; }
 
 MetricResult::ValueType CodeLinesCountMetric::CalculateImpl(const function::Function &f) const {
-    static constexpr std::string_view comment_pattern = "(comment";
+    static auto not_comment = boyer_moore_not_contains("(comment");
 
-    auto res = f.ast | vs::split('\n') | vs::transform([](const auto &range) -> std::optional<int> {
-                   const auto str_view = std::string_view{range};
-
-                   static const std::boyer_moore_searcher searcher(comment_pattern.begin(), comment_pattern.end());
-
-                   if (std::search(str_view.begin(), str_view.end(), searcher) == str_view.end()) {
-                       if (auto start_pos = str_view.find('['); start_pos != std::string_view::npos) {
-                           if (auto comma = str_view.find(',', start_pos); comma != std::string_view::npos) {
-                               return ToInt(str_view.substr(start_pos + 1, comma - start_pos - 1));
-                           }
+    auto res = f.ast | vs::split('\n') | vs::filter(not_comment) |
+               vs::transform([](const auto &range) -> std::optional<int> {
+                   auto str_view = std::string_view{range};
+                   if (auto start_pos = str_view.find('['); start_pos != std::string_view::npos) {
+                       if (auto comma_pos = str_view.find(',', start_pos); comma_pos != std::string_view::npos) {
+                           return ToInt(str_view.substr(start_pos + 1, comma_pos - start_pos - 1));
                        }
                    }
 
