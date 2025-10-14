e28a8b7a273a82a5b4b0da6c8b5bacaf97e66220
huhen <9335143+huhen@users.noreply.github.com>
2025-10-14 17:27:01 +0000

    add boyer moore

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <algorithm>
 #include <charconv>
+#include <functional>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -13,3 +15,25 @@ inline int ToInt(std::string_view value) {
     }
     return result;
 }
+
+template <bool ShouldContain = true>
+class boyer_moore_search_view {
+public:
+    constexpr explicit boyer_moore_search_view(std::string_view pattern) : searcher_(pattern.begin(), pattern.end()) {}
+
+    bool operator()(const auto &range) const {
+        auto result = std::search(range.cbegin(), range.cend(), searcher_);
+        if constexpr (ShouldContain) {
+            return result != range.cend();
+        } else {
+            return result == range.cend();
+        }
+    }
+
+private:
+    const std::boyer_moore_searcher<std::string_view::const_iterator> searcher_;
+};
+
+constexpr auto boyer_moore_contains(std::string_view pattern) { return boyer_moore_search_view<true>{pattern}; }
+
+constexpr auto boyer_moore_not_contains(std::string_view pattern) { return boyer_moore_search_view<false>{pattern}; }
