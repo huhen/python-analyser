7b3a8392144d25132d4125ac9a6f225516a22da9
huhen <9335143+huhen@users.noreply.github.com>
2025-10-18 09:40:14 +0000

    impl

--- src/metric_impl/cyclomatic_complexity.cpp
+++ src/metric_impl/cyclomatic_complexity.cpp
@@ -1,23 +1,51 @@
 #include "metric_impl/cyclomatic_complexity.hpp"
 
+#include <cctype>
 #include <unistd.h>
 
 #include <algorithm>
 #include <array>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <fstream>
-#include <functional>
-#include <iostream>
 #include <ranges>
-#include <sstream>
 #include <string>
-#include <variant>
-#include <vector>
+#include <string_view>
+
+using namespace std::string_view_literals;
 
 namespace analyser::metric::metric_impl {
 
-// здесь ваш код
+std::string CyclomaticComplexityMetric::Name() const { return "cyclomatic_complexity"; }
+
+MetricResult::ValueType CyclomaticComplexityMetric::CalculateImpl(const function::Function &f) const {
+    // clang-format off
+    static constexpr std::array<std::string_view, 12> keywords = {
+        "(if_", 
+        "(else_", 
+        "(elif_", 
+        "(while_", 
+        "(for_", 
+        "(try_", 
+        "(except_", 
+        "(finally_", 
+        "(match_", 
+        "(case_", 
+        "(assert_", 
+        "(conditional_"
+    };
+
+    return static_cast<int>(
+        rs::count_if(
+            f.ast |
+            vs::split('\n') |
+            vs::transform([](const auto &line) {
+                return std::string_view{vs::drop_while(line, ::isspace)};
+            }),
+            [](const auto &line) {
+                return rs::any_of(keywords, [&line](const auto &keyword) {
+                    return line.starts_with(keyword);
+                });
+            })
+        + 1);
+    // clang-format on
+}
 
 }  // namespace analyser::metric::metric_impl
