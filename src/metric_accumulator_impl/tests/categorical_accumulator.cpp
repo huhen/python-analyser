b02c05f30b328a52fe4d5e76f95abb20ec12c809
huhen <9335143+huhen@users.noreply.github.com>
2025-10-20 21:10:16 +0000

    test

--- src/metric_accumulator_impl/tests/categorical_accumulator.cpp
+++ src/metric_accumulator_impl/tests/categorical_accumulator.cpp
@@ -8,6 +8,63 @@
 
 namespace analyser::metric_accumulator::metric_accumulator_impl::test {
 
-// здесь ваш код
+TEST(CategoricalAccumulatorTest, EmptyTest) {
+    CategoricalAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Finalize();
+    auto actual = accumulator.Get();
+    EXPECT_TRUE(actual.empty());
+}
+
+TEST(CategoricalAccumulatorTest, ValidTest) {
+    CategoricalAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", "1"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Finalize();
+    auto actual = accumulator.Get();
+    EXPECT_EQ(actual["1"], 1);
+    EXPECT_EQ(actual["2"], 2);
+}
+
+TEST(CategoricalAccumulatorTest, MustBeFinalizedTest) {
+    CategoricalAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", "1"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    EXPECT_THROW(accumulator.Get(), std::runtime_error);
+}
+
+TEST(CategoricalAccumulatorTest, AccumulateAfterFinalizeTest) {
+    CategoricalAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", "1"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Finalize();
+    EXPECT_THROW(accumulator.Accumulate(metric::MetricResult{"", "3"}), std::runtime_error);
+}
+
+TEST(CategoricalAccumulatorTest, ComputeAfterResetTest) {
+    CategoricalAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", "1"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Finalize();
+    auto actual = accumulator.Get();
+    EXPECT_EQ(actual["1"], 1);
+    EXPECT_EQ(actual["2"], 2);
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", "1"});
+    accumulator.Accumulate(metric::MetricResult{"", "1"});
+    accumulator.Accumulate(metric::MetricResult{"", "2"});
+    accumulator.Finalize();
+    actual = accumulator.Get();
+    EXPECT_EQ(actual["1"], 2);
+    EXPECT_EQ(actual["2"], 1);
+}
 
 }  // namespace analyser::metric_accumulator::metric_accumulator_impl::test
--- src/metric_accumulator_impl/tests/sum_average_accumulator.cpp
+++ src/metric_accumulator_impl/tests/sum_average_accumulator.cpp
@@ -2,10 +2,53 @@
 
 #include <gtest/gtest.h>
 
-#include <cmath>
-
 namespace analyser::metric_accumulator::metric_accumulator_impl::test {
 
-// здесь ваш код
+TEST(SumAverageAccumulatorTest, EmptyTest) {
+    SumAverageAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Finalize();
+    EXPECT_TRUE(accumulator.Get() == SumAverageAccumulator::SumAverage(0, 0));
+}
+
+TEST(SumAverageAccumulatorTest, ValidTest) {
+    SumAverageAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", 1});
+    accumulator.Accumulate(metric::MetricResult{"", 2});
+    accumulator.Finalize();
+    EXPECT_TRUE(accumulator.Get() == SumAverageAccumulator::SumAverage(3, 1.5));
+}
+
+TEST(SumAverageAccumulatorTest, MustBeFinalizedTest) {
+    SumAverageAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", 1});
+    accumulator.Accumulate(metric::MetricResult{"", 2});
+    EXPECT_THROW(accumulator.Get(), std::runtime_error);
+}
+
+TEST(SumAverageAccumulatorTest, AccumulateAfterFinalizeTest) {
+    SumAverageAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", 1});
+    accumulator.Accumulate(metric::MetricResult{"", 2});
+    accumulator.Finalize();
+    EXPECT_THROW(accumulator.Accumulate(metric::MetricResult{"", 3}), std::runtime_error);
+}
+
+TEST(SumAverageAccumulatorTest, ComputeAfterResetTest) {
+    SumAverageAccumulator accumulator;
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", 1});
+    accumulator.Accumulate(metric::MetricResult{"", 2});
+    accumulator.Finalize();
+    EXPECT_TRUE(accumulator.Get() == SumAverageAccumulator::SumAverage(3, 1.5));
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"", 3});
+    accumulator.Accumulate(metric::MetricResult{"", 5});
+    accumulator.Finalize();
+    EXPECT_TRUE(accumulator.Get() == SumAverageAccumulator::SumAverage(8, 4));
+}
 
 }  // namespace analyser::metric_accumulator::metric_accumulator_impl::test
